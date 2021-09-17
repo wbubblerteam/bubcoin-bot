45f09476122a5332795361c248720912f104370c
JMcB <joel.mcbride1@live.com>
2021-09-17 19:57:52 +0100

    Start creating the bot class, models etc

--- bot.py
+++ bot.py
@@ -1,10 +1,65 @@
 #!/usr/bin/env python3
 
+import json
+
+import discord
+from discord.ext import commands
+from sqlalchemy import Column, Integer, Text
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+
+
 __version__ = '0.1.0'
+
+CONFIG_PATH = 'config.json'
+DB_PATH = 'bubcoinbot.db'
+DEFAULT_PREFIX = 'B$'
+SQL_ECHO = True
+
+
+# orm mappings
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    discord_id = Column(Integer, primary_key=True)
+    bubcoin_address = Column(Text)
+    # The discord id signed with the private key of the address
+    bubcoin_signature = Column(Text)
+
+
+# bot stuff
+class BubcoinBot(commands.Bot):
+    def __init__(self, *args, **kwargs):
+        self.add_cog(BubcoinBotCommands(self))
+
+        self.engine = create_async_engine(f'sqlite+aiosqlite:///{DB_PATH}', echo=SQL_ECHO, future=True)
+        self.session = AsyncSession(self.engine)
+
+        super().__init__(*args, **kwargs)
+
+    def close(self):
+        self.session.close()
+        super().close()
+
+
+class BubcoinBotCommands(commands.Cog):
+    def __init__(self, bot: BubcoinBot):
+        self.bot = bot
+
+    @commands.command()
+    def myid(self, ctx: commands.Context):
+        return await ctx.send(ctx.author.id)
 
 
 def main():
-    pass
+    with open(CONFIG_PATH) as config_file:
+        config = json.load(config_file)
+
+    bubcoin_bot = BubcoinBot(command_prefix=DEFAULT_PREFIX)
+    bubcoin_bot.run(config['token'])
 
 
 if __name__ == '__main__':
