1442a3586c76ec53794577d4888d3c43437f5b0f
JMcB <joel.mcbride1@live.com>
2021-09-19 13:56:16 +0100

    Write the send_bubcoin commands
    Includes fixes to database code for the verify command, and a minor refactor to let the cog access the sqlalchemy session directly.

--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ legitimate, commercial, high proof-of-work
 
 
 import json
+from decimal import Decimal
 from typing import Optional
 
 import aiohttp
@@ -33,9 +34,14 @@ BOT_PERMISSIONS = {
 }
 DEFAULT_PREFIX = ('B$', 'b$', '$')
 COIN = 100000000
+MAX_MONEY = 1000 * COIN
 RPC_PORT = 8332
 RPC_USERNAME = 'user'
 RPC_ID = 'bubcoinbot'
+
+
+def coin(prettytinybubs: int) -> Decimal:
+    return Decimal(prettytinybubs) / Decimal(COIN)
 
 
 # orm mappings
@@ -77,6 +83,9 @@ class BubcoinBot(commands.Bot):
 
     async def on_ready(self):
         self.aioh_session = aiohttp.ClientSession()
+        async with self.sqla_engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
+
         print(f'Started as {self.user.name}.')
 
     async def on_command_error(self, ctx: commands.Context, exception: Exception):
@@ -100,6 +109,9 @@ class BubcoinBotCommands(commands.Cog):
     def __init__(self, bot: BubcoinBot):
         self.bot = bot
         self.rpc_url = f'http://127.0.0.1:{RPC_PORT}/'
+
+        self.sqla_session = self.bot.sqla_session
+        self.aioh_session = self.bot.aioh_session
 
     @commands.command(aliases=['github', 'git', 'source'])
     async def github_url(self, ctx: commands.Context):
@@ -146,7 +158,7 @@ class BubcoinBotCommands(commands.Cog):
             'params': params,
         }
 
-        async with self.bot.aioh_session.get(
+        async with self.aioh_session.get(
             self.rpc_url, json=json_data, headers=headers, auth=aiohttp.BasicAuth(RPC_USERNAME)
         ) as response:
             return await response.json()
@@ -171,6 +183,8 @@ IIib3x/iuYuhUxAeiDO2i+F3Kz4idLVNK5OlEwp3991WNWy9mTl4RZRGOw2weA3tlsDHYag3zKt9I3EO
         For example:
         `bubcoin-cli signmessage "bcrt1qdm8hufy56erp5mf5epqevw5u4mywn9j0tpm3ke" "329885271787307008"`
         """
+        # todo: regex sanity checks for args
+
         address_validation = await self.rpc_call('validateaddress', address)
         valid_address = address_validation['isvalid']
         if not valid_address:
@@ -180,9 +194,12 @@ IIib3x/iuYuhUxAeiDO2i+F3Kz4idLVNK5OlEwp3991WNWy9mTl4RZRGOw2weA3tlsDHYag3zKt9I3EO
         if not verified_message:
             return await ctx.send('Invalid cryptographic signature.')
 
-        user: User = await self.bot.sqla_session.get(User, ctx.author.id)
-        prev_address = user.bubcoin_address
-        async with self.bot.sqla_session.begin():
+        user = await self.sqla_session.get(User, ctx.author.id)
+        async with self.sqla_session.begin():
+            if user is None:
+                user = User(ctx.author.id)
+                await self.sqla_session.add(user)
+            prev_address = user.bubcoin_address
             user.bubcoin_address = address
             user.bubcoin_signature = signature
 
@@ -190,6 +207,42 @@ IIib3x/iuYuhUxAeiDO2i+F3Kz4idLVNK5OlEwp3991WNWy9mTl4RZRGOw2weA3tlsDHYag3zKt9I3EO
         if prev_address is not None:
             message = f'Your previous address was {prev_address}.\n' + message
         return await ctx.send(message)
+
+    @commands.command(aliases=['send', 'send_bubcoins'])
+    async def send_bubcoin(self, ctx: commands.Context, user: discord.User, amount: Decimal):
+        amount_prettytinybubs = int(amount * COIN)
+        # sanity check
+        if amount_prettytinybubs > MAX_MONEY:
+            return await ctx.send(
+                "You can't send more than the maximum possible number of Bubcoins. \n"
+                f'Max Bubcoins: \n₿{coin(MAX_MONEY)}'
+            )
+
+        # check if sender has an account
+        sender = await self.sqla_session.get(User, ctx.author.id)
+        if sender is None:
+            return await ctx.send(f'You do not have a Bubcoin Bot account. Try: \n{ctx.prefix}verify')
+
+        # check if sender has enough money
+        if amount_prettytinybubs > sender.prettytinybubs:
+            return await ctx.send(
+                'Insufficient funds. \n'
+                f'You have: \n₿{coin(sender.prettytinybubs)}\n'
+                f'You would need an additional: ₿{coin(amount_prettytinybubs - sender.prettytinybubs)}\n'
+            )
+
+        # send the money
+        recipient = await self.sqla_session.get(User, user.id)
+        async with self.sqla_session.begin():
+            # make an account for recipient if needed
+            if recipient is None:
+                recipient = User(user.id)
+                self.sqla_session.add(recipient)
+            sender.prettytinybubs -= amount_prettytinybubs
+            recipient.prettytinybubs += amount_prettytinybubs
+        return await ctx.send(
+            f'Transaction successful! \nYour new balance is: \n{sender.prettytinybubs}'
+        )
 
 
 def main():
